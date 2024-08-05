954aab030edf289d568176b3850a5f8600e47c4b
Santos2175 <santoshgurung949@gmail.com>
2024-08-05 08:18:27 +0545

    Initial setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express  = require('express')
+
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/',(req,res)=>res.send('Hello'))
+
+app.listen(PORT, ()=>console.log("server started at PORT: ", PORT))
+
