123f24f10f68613fa5164161834aaab330d49fba
Santos2175 <santoshgurung949@gmail.com>
2024-08-29 23:48:05 +0545

    profile reducer added

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import propTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = (props) => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
   return (
     <div className='container'>
       <h2>Dashboard</h2>
@@ -9,6 +15,15 @@ const Dashboard = (props) => {
   );
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.auth,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Navigate, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const PrivateRoute = ({
-  component: Component,
-  auth: { isAuthenticated, loading },
-}) => {
+const PrivateRoute = ({ component: Component, auth: { isAuthenticated } }) => {
   if (isAuthenticated) return <Component />;
 
   return <Navigate to='/login' />;
