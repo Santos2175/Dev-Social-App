58a788c4f4cbb835bd2b232d613f6e90e2a6b0c9
Santos2175 <santoshgurung949@gmail.com>
2024-09-14 18:52:11 +0545

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./db/db.js');
 const cors = require('cors');
+const path = require('path');
 
 const PORT = process.env.REACT_APP_PORT || 8000;
+
+console.log(__dirname);
 
 const app = express();
 
@@ -19,5 +22,11 @@ app.use('/api/users', require('./routes/api/users.js'));
 app.use('/api/auth', require('./routes/api/auth.js'));
 app.use('/api/profile', require('./routes/api/profile.js'));
 app.use('/api/posts', require('./routes/api/posts.js'));
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => console.log('server started at PORT: ', PORT));
