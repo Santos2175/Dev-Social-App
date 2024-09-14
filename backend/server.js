2aa92020f350fdb82233b9762080d8f18a0a3088
Santos2175 <santoshgurung949@gmail.com>
2024-09-14 19:00:21 +0545

    Deployment bug fix

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use('/api/posts', require('./routes/api/posts.js'));
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log('server started at PORT: ', PORT));
